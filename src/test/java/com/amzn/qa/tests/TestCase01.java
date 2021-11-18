fadee73f5949a288de9916a1c4efed48ee7eddb7
howlett-james <yokeshwararao@hotmail.com>
2021-11-18 10:52:48 +0530

    Revert "Initial Commit"
    This reverts commit 2864b24224b13b7d3a921ebb5eed925429392647.

--- src/test/java/com/amzn/qa/tests/TestCase01.java
+++ src/test/java/com/amzn/qa/tests/TestCase01.java
@@ -3,9 +3,7 @@ package com.amzn.qa.tests;
 import com.amzn.qa.base.TestBase;
 import com.amzn.qa.pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestCase01 extends TestBase {
     LoginPage loginPage;
