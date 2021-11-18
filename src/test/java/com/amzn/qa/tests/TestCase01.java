2864b24224b13b7d3a921ebb5eed925429392647
howlett-james <yokeshwararao@hotmail.com>
2021-11-18 10:47:04 +0530

    Initial Commit

--- src/test/java/com/amzn/qa/tests/TestCase01.java
+++ src/test/java/com/amzn/qa/tests/TestCase01.java
@@ -3,7 +3,9 @@ package com.amzn.qa.tests;
 import com.amzn.qa.base.TestBase;
 import com.amzn.qa.pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
 
 public class TestCase01 extends TestBase {
     LoginPage loginPage;
